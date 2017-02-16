9c807b7f43f09c98c59afabf89fb965d871d5f68
Eugene Glova <eugene.glova@gmail.com>
2017-02-16 19:47:17 +0300

    Add progres for webpack, redux, react-redux and redux-thunk

--- static/js/src/index.js
+++ static/js/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './containers/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(() => {}, composeEnhancers(applyMiddleware(thunk)));
+
 render(
-	<App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
