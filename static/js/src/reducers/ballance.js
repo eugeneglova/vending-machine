a6d058b0bb1f911e32cc48dfbd4db808ef1d2623
Eugene Glova <eugene.glova@gmail.com>
2017-02-17 12:58:28 +0300

    Fix: add semicolon

--- static/js/src/reducers/ballance.js
+++ static/js/src/reducers/ballance.js
@@ -8,10 +8,10 @@ const initialState = 0;
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case INCREMENT_BALLANCE:
-			return state + action.payload
+			return state + action.payload;
 			break;
 		case DECREMENT_BALLANCE:
-			return state - action.payload
+			return state - action.payload;
 			break;
 		default:
 			return state;
