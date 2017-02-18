21df6cf733e84ae33cdebeead48f391910f47915
Eugene Glova <eugene.glova@gmail.com>
2017-02-18 14:42:59 +0300

    Show loading products

--- static/js/src/containers/ProductList.js
+++ static/js/src/containers/ProductList.js
@@ -7,11 +7,15 @@ import { buyProduct } from '../actions';
 const ProductList = (
 	({ products, balance, onProductClick }) => (
 		<section className="row">
-			{map(products.data, product => (
-				<div key={product.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
-					<ProductListItem product={product} balance={balance} onClick={onProductClick} />
-				</div>
-			))}
+			{
+				products.loading ?
+				'Loading products...' :
+				map(products.data, product => (
+					<div key={product.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
+						<ProductListItem product={product} balance={balance} onClick={onProductClick} />
+					</div>
+				))
+			}
 		</section>
 	)
 );
